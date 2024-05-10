956c6d2716162cece4eb978e56eb7766802aca47
XPoet <i@xpoet.cn>
2024-05-10 23:00:35 +0800

    build: integrated `eslint`

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import pluginJs from '@eslint/js'
+import pluginPrettier from 'eslint-plugin-prettier/recommended'
+
+export default [
+  pluginJs.configs.recommended,
+  pluginPrettier,
+  {
+    rules: {},
+    languageOptions: {
+      globals: {
+        console: true
+      }
+    },
+    // ignore files
+    ignores: ['.git', '.vscode', '.idea', '.husky', 'node_modules', 'dist']
+  }
+]
