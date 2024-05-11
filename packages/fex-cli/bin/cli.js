e978ad71209bc3513000b019b1dfb5c88ec51db1
XPoet <i@xpoet.cn>
2024-05-11 15:30:57 +0800

    feat: create cli commands using `commander`

--- packages/fex-cli/bin/cli.js
+++ packages/fex-cli/bin/cli.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node
 
-console.log('hello fex-cli')
+import { Command } from 'commander'
+import { CLI_NAME, DESC, VERSION } from '../src/constant.js'
+
+const program = new Command()
+
+program.name(CLI_NAME).description(DESC).version(VERSION).usage('[command] [options]')
+
+program
+  .command('create')
+  .description('create a new project')
+  .argument('<name>', 'project name')
+  .option('-f, --force', 'overwrite project directory if it already exists')
+  .action((name, option) => {
+    console.log('name: ', name)
+    console.log('options: ', option)
+  })
+
+program.parse()
---
+++ packages/fex-cli/src/constant.js
@@ -0,0 +1,7 @@
+import version from './version.cjs'
+
+export const VERSION = version
+
+export const CLI_NAME = 'fex-cli'
+
+export const DESC = 'CLI to create a front-end project template'
